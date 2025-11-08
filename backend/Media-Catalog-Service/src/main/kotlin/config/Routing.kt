68210ddab440e1b72662d16557ae84a38ea5cfe8
nicoszolderits <mail@nszo.net>
2025-11-08 16:40:31 +0100

    refactor: moved routing from Application.kt to Routing.kt

--- backend/Media-Catalog-Service/src/main/kotlin/config/Routing.kt
+++ backend/Media-Catalog-Service/src/main/kotlin/config/Routing.kt
@@ -1,24 +1,13 @@
 package com.collektar
 
-import com.auth0.jwt.JWT
-import com.auth0.jwt.algorithms.Algorithm
+import controllers.ControllerRegistry
 import io.ktor.http.*
-import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
-import io.ktor.server.auth.*
-import io.ktor.server.auth.jwt.*
-import io.ktor.server.metrics.micrometer.*
-import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.plugins.cors.routing.*
-import io.ktor.server.plugins.defaultheaders.*
 import io.ktor.server.plugins.statuspages.*
-import io.ktor.server.plugins.swagger.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import io.micrometer.prometheus.*
-import org.koin.dsl.module
-import org.koin.ktor.plugin.Koin
-import org.koin.logger.slf4jLogger
+import org.koin.ktor.ext.get
+
 
 fun Application.configureRouting() {
     install(StatusPages) {
@@ -26,10 +15,12 @@ fun Application.configureRouting() {
             call.respondText(text = "500: $cause", status = HttpStatusCode.InternalServerError)
         }
     }
+
+    val registry: ControllerRegistry = get()
+
     routing {
-        get("/hello") {
-            call.respondText("Hello from Media-Catalog Service")
-        }
+        registry.registerAll(this)
+
         get("/health") {
             call.respondText("OK", ContentType.Text.Plain, HttpStatusCode.OK)
         }
