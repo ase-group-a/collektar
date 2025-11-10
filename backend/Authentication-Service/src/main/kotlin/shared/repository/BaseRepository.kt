705e92615ce5c1ce9d3ed4474096d571b26e028e
JL <115737916+yikesboy@users.noreply.github.com>
2025-11-10 21:53:33 +0100

    feat: created transaction wrapper

--- backend/Authentication-Service/src/main/kotlin/shared/repository/BaseRepository.kt
+++ backend/Authentication-Service/src/main/kotlin/shared/repository/BaseRepository.kt
@@ -3,10 +3,17 @@ package com.collektar.shared.repository
 import kotlinx.coroutines.Dispatchers
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
+import org.jetbrains.exposed.sql.transactions.transaction
 
 abstract class BaseRepository(
     protected val database: Database
 ) {
     protected suspend fun <T> dbQuery(block: suspend () -> T): T =
         newSuspendedTransaction(Dispatchers.IO) { block() }
+
+    protected suspend fun <T> dbTransaction(block: suspend () -> T): T = dbQuery {
+        transaction {
+            block()
+        }
+    }
 }
