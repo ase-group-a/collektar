d7bd765e525db7ae4320a3dc695762dbc60183bc
JL <115737916+yikesboy@users.noreply.github.com>
2025-11-10 11:21:19 +0100

    feat: added first auth routes and setup statuspage exception catching

--- backend/Authentication-Service/src/main/kotlin/Routing.kt
+++ backend/Authentication-Service/src/main/kotlin/Routing.kt
@@ -1,7 +1,9 @@
 package com.collektar
 
-import com.auth0.jwt.JWT
-import com.auth0.jwt.algorithms.Algorithm
+import com.collektar.DTOs.ErrorResponse
+import com.collektar.features.auth.IAuthService
+import com.collektar.features.auth.authRoutes
+import com.collektar.shared.errors.AppError
 import io.ktor.client.*
 import io.ktor.client.engine.apache.*
 import io.ktor.http.*
@@ -19,19 +21,28 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.sessions.*
 import io.micrometer.prometheus.*
-import java.sql.Connection
-import java.sql.DriverManager
 import org.jetbrains.exposed.sql.*
-import org.koin.dsl.module
-import org.koin.ktor.plugin.Koin
-import org.koin.logger.slf4jLogger
+import org.koin.ktor.ext.get
 
 fun Application.configureRouting() {
+    val authService = get<IAuthService>()
+
     install(StatusPages) {
+        exception<AppError> { call, cause ->
+            call.respond(
+                cause.statusCode,
+                ErrorResponse(message = cause.message ?: "Unexpected error occurred.")
+            )
+        }
         exception<Throwable> { call, cause ->
-            call.respondText(text = "500: $cause", status = HttpStatusCode.InternalServerError)
+            call.application.log.error("Unhandled exception caught", cause)
+            call.respond(
+                HttpStatusCode.InternalServerError,
+                ErrorResponse(message = "Internal Server Error: $cause")
+            )
         }
     }
+
     routing {
         get("/hello") {
             call.respondText("Hello from Authentication Service")
@@ -40,5 +51,6 @@ fun Application.configureRouting() {
         get("/health") {
             call.respondText("OK", ContentType.Text.Plain, HttpStatusCode.OK)
         }
+        authRoutes(authService)
     }
 }
