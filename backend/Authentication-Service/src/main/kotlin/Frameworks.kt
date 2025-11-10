0396a42bce79c9416a6146989e84f7e3a84dca7c
JL <115737916+yikesboy@users.noreply.github.com>
2025-11-10 11:21:44 +0100

    feat: setup first basic dependency injection

--- backend/Authentication-Service/src/main/kotlin/Frameworks.kt
+++ backend/Authentication-Service/src/main/kotlin/Frameworks.kt
@@ -1,27 +1,17 @@
 package com.collektar
 
-import com.auth0.jwt.JWT
-import com.auth0.jwt.algorithms.Algorithm
-import io.ktor.client.*
-import io.ktor.client.engine.apache.*
-import io.ktor.http.*
-import io.ktor.serialization.kotlinx.json.*
+
+import com.collektar.config.JWTConfig
+import com.collektar.features.auth.AuthRepository
+import com.collektar.features.auth.AuthService
+import com.collektar.features.auth.IAuthRepository
+import com.collektar.features.auth.IAuthService
+import com.collektar.shared.database.DatabaseFactory
+import com.collektar.shared.security.Hasher.BCryptHasher
+import com.collektar.shared.security.JWTService.IJWTService
+import com.collektar.shared.security.JWTService.JWTService
+import com.collektar.shared.security.PasswordHasher.IPasswordHasher
 import io.ktor.server.application.*
-import io.ktor.server.auth.*
-import io.ktor.server.auth.jwt.*
-import io.ktor.server.metrics.micrometer.*
-import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.plugins.cors.routing.*
-import io.ktor.server.plugins.statuspages.*
-import io.ktor.server.plugins.swagger.*
-import io.ktor.server.request.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
-import io.ktor.server.sessions.*
-import io.micrometer.prometheus.*
-import java.sql.Connection
-import java.sql.DriverManager
-import org.jetbrains.exposed.sql.*
 import org.koin.dsl.module
 import org.koin.ktor.plugin.Koin
 import org.koin.logger.slf4jLogger
@@ -29,12 +19,16 @@ import org.koin.logger.slf4jLogger
 fun Application.configureFrameworks() {
     install(Koin) {
         slf4jLogger()
+
+        val jwtConfig = JWTConfig.fromEnv()
+        val database = DatabaseFactory.create()
         modules(module {
-            single<HelloService> {
-                HelloService {
-                    println(environment.log.info("Hello, World!"))
-                }
-            }
+            single { jwtConfig }
+            single { database }
+            single<IJWTService> { JWTService(get()) }
+            single<IPasswordHasher> { BCryptHasher() }
+            single<IAuthRepository> { AuthRepository(get()) }
+            single<IAuthService> { AuthService(get(), get(), get()) }
         })
     }
 }
