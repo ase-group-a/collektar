ab2681b5f86d2a75e7619fee7bfe3ccd1a87310c
JL <115737916+yikesboy@users.noreply.github.com>
2025-11-10 11:10:36 +0100

    build: added jbcrypt to hash passwords

--- backend/Authentication-Service/build.gradle.kts
+++ backend/Authentication-Service/build.gradle.kts
@@ -43,6 +43,8 @@ dependencies {
     implementation("io.ktor:ktor-server-netty")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-config-yaml")
+    implementation("org.mindrot:jbcrypt:0.4")
+    implementation("io.ktor:ktor-serialization-gson:3.3.1")
     testImplementation("io.ktor:ktor-server-test-host")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
