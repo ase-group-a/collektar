6a81bb738eea5e34664d0309836b0f18f95f75ae
JL <115737916+yikesboy@users.noreply.github.com>
2025-11-14 23:18:46 +0100

    feat: update dependency injection setup

--- backend/Authentication-Service/src/main/kotlin/Frameworks.kt
+++ backend/Authentication-Service/src/main/kotlin/Frameworks.kt
@@ -2,6 +2,7 @@ package com.collektar
 
 
 import com.collektar.config.JWTConfig
+import com.collektar.config.TokenHasherConfig
 import com.collektar.features.auth.AuthRepository
 import com.collektar.features.auth.AuthService
 import com.collektar.features.auth.IAuthRepository
@@ -11,6 +12,10 @@ import com.collektar.shared.security.JWTService.IJWTService
 import com.collektar.shared.security.JWTService.JWTService
 import com.collektar.shared.security.PasswordHasher.BCryptHasher
 import com.collektar.shared.security.PasswordHasher.IPasswordHasher
+import com.collektar.shared.security.RefreshTokenHasher.HmacTokenHasher
+import com.collektar.shared.security.RefreshTokenHasher.IRefreshTokenHasher
+import com.collektar.shared.security.tokenservice.ITokenService
+import com.collektar.shared.security.tokenservice.TokenService
 import io.ktor.server.application.*
 import org.koin.dsl.module
 import org.koin.ktor.plugin.Koin
@@ -21,13 +26,17 @@ fun Application.configureFrameworks() {
         slf4jLogger()
 
         val jwtConfig = JWTConfig.fromEnv()
+        val tokenHasherConfig = TokenHasherConfig.fromEnv()
         val database = DatabaseFactory.create()
         modules(module {
             single { jwtConfig }
+            single { tokenHasherConfig }
             single { database }
             single<IJWTService> { JWTService(get()) }
             single<IPasswordHasher> { BCryptHasher() }
+            single<IRefreshTokenHasher> { HmacTokenHasher(get()) }
             single<IAuthRepository> { AuthRepository(get()) }
+            single<ITokenService> { TokenService(get(), get(), get()) }
             single<IAuthService> { AuthService(get(), get(), get()) }
         })
     }
