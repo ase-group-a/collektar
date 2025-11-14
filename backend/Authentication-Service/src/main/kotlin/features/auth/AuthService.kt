c0af690b26490930c5a73f97fea4f04cf45182e2
JL <115737916+yikesboy@users.noreply.github.com>
2025-11-14 23:18:16 +0100

    refactor: refactored AuthService to use TokenService

--- backend/Authentication-Service/src/main/kotlin/features/auth/AuthService.kt
+++ backend/Authentication-Service/src/main/kotlin/features/auth/AuthService.kt
@@ -1,19 +1,16 @@
 package com.collektar.features.auth
 
-import com.auth0.jwt.interfaces.DecodedJWT
 import com.collektar.dto.*
 import com.collektar.shared.errors.AppError
-import com.collektar.shared.security.JWTService.IJWTService
-import com.collektar.shared.security.JWTService.RefreshToken
 import com.collektar.shared.security.PasswordHasher.IPasswordHasher
-import java.time.Duration
-import java.time.Instant
+import com.collektar.shared.security.tokenservice.ITokenService
+import com.collektar.shared.security.tokenservice.TokenPair
 import java.util.*
 
 class AuthService(
     private val repository: IAuthRepository,
-    private val jwtService: IJWTService,
-    private val passwordHasher: IPasswordHasher
+    private val tokenService: ITokenService,
+    private val passwordHasher: IPasswordHasher,
 ) : IAuthService {
     override suspend fun register(request: RegisterRequest): RegisterResponse {
         if (repository.usernameExists(request.username)) {
@@ -21,26 +18,20 @@ class AuthService(
         }
 
         if (repository.emailExists(request.email)) {
-            throw AppError.Conflict.EmailAlreadyInUse(request.email)
+            throw AppError.Conflict.EmailAlreadyInUse()
         }
 
         val userId = UUID.randomUUID()
         val passwordHash = passwordHasher.hash(request.password)
-        val accessToken = jwtService.generateAccessToken(userId, request.email)
-        val refreshToken = jwtService.generateRefreshToken(userId)
-        val expiresIn: Long = Duration.between(Instant.now(), accessToken.expiresAt).toMillis()
-        val refreshTokenExpiresIn: Long = Duration.between(Instant.now(), refreshToken.expiresAt).toMillis()
+        repository.createUser(
+            userId = userId,
+            username = request.username,
+            email = request.email,
+            displayName = request.displayName,
+            passwordHash = passwordHash,
+        )
 
-        repository.transaction {
-            repository.createUser(
-                userId = userId,
-                username = request.username,
-                email = request.email,
-                displayName = request.displayName,
-                passwordHash = passwordHash,
-            )
-            saveRefreshToken(userId = userId, token = refreshToken)
-        }
+        val tokenPair = tokenService.generateTokens(userId = userId, email = request.email)
 
         val userInfo = UserInfo(
             email = request.email,
@@ -49,10 +40,10 @@ class AuthService(
         )
 
         return RegisterResponse(
-            accessToken = accessToken.token,
-            expiresIn = expiresIn,
-            refreshToken = refreshToken.token,
-            refreshTokenExpiresIn = refreshTokenExpiresIn,
+            accessToken = tokenPair.accessToken,
+            expiresIn = tokenPair.accessTokenExpiresIn,
+            refreshToken = tokenPair.refreshToken,
+            refreshTokenExpiresIn = tokenPair.refreshTokenExpiresIn,
             user = userInfo
         )
     }
@@ -66,44 +57,24 @@ class AuthService(
             throw AppError.Unauthorized.InvalidCredentials()
         }
 
-        return authenticateUser(userId = user.id, email = user.email)
-    }
-
-    override suspend fun refresh(request: RefreshTokenRequest): AuthenticationResponse {
-        val decodedJWT: DecodedJWT = jwtService.verify(
-            token = request.refreshToken
-        ) ?: throw AppError.Unauthorized.InvalidToken()
-
-        val userId = UUID.fromString(decodedJWT.subject)
-        val user = repository.findByUserId(
-            userId = userId
-        ) ?: throw AppError.Unauthorized.InvalidToken()
-
-        return authenticateUser(userId = user.id, email = user.email)
-    }
-
-    private suspend fun authenticateUser(userId: UUID, email: String): AuthenticationResponse {
-        val accessToken = jwtService.generateAccessToken(userId = userId, email = email)
-        val expiresIn: Long = Duration.between(Instant.now(), accessToken.expiresAt).toMillis()
-        val refreshToken = jwtService.generateRefreshToken(userId = userId)
-        val refreshTokenExpiresIn: Long = Duration.between(Instant.now(), refreshToken.expiresAt).toMillis()
-
-        saveRefreshToken(userId = userId, token = refreshToken)
+        val tokenPair: TokenPair = tokenService.generateTokens(userId = user.id, email = user.email)
 
         return AuthenticationResponse(
-            accessToken = accessToken.token,
-            expiresIn = expiresIn,
-            refreshToken = refreshToken.token,
-            refreshTokenExpiresIn = refreshTokenExpiresIn
+            accessToken = tokenPair.accessToken,
+            expiresIn = tokenPair.accessTokenExpiresIn,
+            refreshToken = tokenPair.refreshToken,
+            refreshTokenExpiresIn = tokenPair.refreshTokenExpiresIn
         )
     }
 
-    private suspend fun saveRefreshToken(userId: UUID, token: RefreshToken) {
-        repository.saveRefreshToken(
-            userId = userId,
-            token = token.token,
-            expiresAt = token.expiresAt,
-            issuedAt = token.issuedAt
+    override suspend fun refresh(request: RefreshTokenRequest): AuthenticationResponse {
+        val tokenPair = tokenService.validateAndRefresh(request.refreshToken)
+
+        return AuthenticationResponse(
+            accessToken = tokenPair.accessToken,
+            expiresIn = tokenPair.accessTokenExpiresIn,
+            refreshToken = tokenPair.refreshToken,
+            refreshTokenExpiresIn = tokenPair.refreshTokenExpiresIn
         )
     }
 }
