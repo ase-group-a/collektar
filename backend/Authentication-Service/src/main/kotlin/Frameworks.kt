cc33b7186b6beb19adc6ddb937b2f39095531ee7
JL <115737916+yikesboy@users.noreply.github.com>
2025-11-14 23:49:24 +0100

    feat: updated dependency injection setup

--- backend/Authentication-Service/src/main/kotlin/Frameworks.kt
+++ backend/Authentication-Service/src/main/kotlin/Frameworks.kt
@@ -2,6 +2,7 @@ package com.collektar
 
 
 import com.collektar.config.JWTConfig
+import com.collektar.config.OpaqueTokenConfig
 import com.collektar.config.TokenHasherConfig
 import com.collektar.features.auth.AuthRepository
 import com.collektar.features.auth.AuthService
@@ -14,6 +15,8 @@ import com.collektar.shared.security.PasswordHasher.BCryptHasher
 import com.collektar.shared.security.PasswordHasher.IPasswordHasher
 import com.collektar.shared.security.RefreshTokenHasher.HmacTokenHasher
 import com.collektar.shared.security.RefreshTokenHasher.IRefreshTokenHasher
+import com.collektar.shared.security.opaquetokengeneration.IOpaqueTokenGenerator
+import com.collektar.shared.security.opaquetokengeneration.OpaqueTokenGenerator
 import com.collektar.shared.security.tokenservice.ITokenService
 import com.collektar.shared.security.tokenservice.TokenService
 import io.ktor.server.application.*
@@ -27,16 +30,19 @@ fun Application.configureFrameworks() {
 
         val jwtConfig = JWTConfig.fromEnv()
         val tokenHasherConfig = TokenHasherConfig.fromEnv()
+        val opaqueTokenConfig = OpaqueTokenConfig()
         val database = DatabaseFactory.create()
         modules(module {
             single { jwtConfig }
             single { tokenHasherConfig }
+            single { opaqueTokenConfig }
             single { database }
             single<IJWTService> { JWTService(get()) }
             single<IPasswordHasher> { BCryptHasher() }
             single<IRefreshTokenHasher> { HmacTokenHasher(get()) }
+            single<IOpaqueTokenGenerator> { OpaqueTokenGenerator(get()) }
             single<IAuthRepository> { AuthRepository(get()) }
-            single<ITokenService> { TokenService(get(), get(), get()) }
+            single<ITokenService> { TokenService(get(), get(), get(), get()) }
             single<IAuthService> { AuthService(get(), get(), get()) }
         })
     }
