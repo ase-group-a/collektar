b7d1f16ffb244e9beaa985f12b6029fae3618e19
nicoszolderits <mail@nszo.net>
2025-11-16 02:39:32 +0100

    refactor: simplified spotify config

--- backend/Media-Catalog-Service/src/main/kotlin/integration/spotify/SpotifyConfig.kt
+++ backend/Media-Catalog-Service/src/main/kotlin/integration/spotify/SpotifyConfig.kt
@@ -11,27 +11,20 @@ data class SpotifyConfig(
 ) {
     companion object {
         fun fromEnv(env: ApplicationEnvironment): SpotifyConfig {
-            val clientId = System.getenv("SPOTIFY_CLIENT_ID")
-                ?: env.config.propertyOrNull("spotify.clientId")?.getString()
-                ?: error("SPOTIFY_CLIENT_ID not set")
 
-            val clientSecret = System.getenv("SPOTIFY_CLIENT_SECRET")
-                ?: env.config.propertyOrNull("spotify.clientSecret")?.getString()
-                ?: error("SPOTIFY_CLIENT_SECRET not set")
+            fun get(keyEnv: String, keyConf: String): String =
+                env.config.propertyOrNull(keyConf)?.getString()
+                    ?: System.getenv(keyEnv)
+                    ?: error("$keyEnv not set")
 
-            val baseUrl = System.getenv("SPOTIFY_BASE_URL")
-                ?: env.config.propertyOrNull("spotify.baseUrl")?.getString()
-                ?: error("SPOTIFY_BASE_URL not set")
-
-            val tokenUrl = System.getenv("SPOTIFY_TOKEN_URL")
-                ?: env.config.propertyOrNull("spotify.tokenUrl")?.getString()
-                ?: error("SPOTIFY_TOKEN_URL not set")
-
-            val defaultPlaylistId = System.getenv("SPOTIFY_DEFAULT_PLAYLIST_ID")
-                ?: env.config.propertyOrNull("spotify.defaultPlaylistId")?.getString()
-                ?: error("SPOTIFY_DEFAULT_PLAYLIST_ID not set")
-
-            return SpotifyConfig(clientId, clientSecret, baseUrl, tokenUrl, defaultPlaylistId)
+            return SpotifyConfig(
+                clientId = get("SPOTIFY_CLIENT_ID", "spotify.clientId"),
+                clientSecret = get("SPOTIFY_CLIENT_SECRET", "spotify.clientSecret"),
+                baseUrl = get("SPOTIFY_BASE_URL", "spotify.baseUrl"),
+                tokenUrl = get("SPOTIFY_TOKEN_URL", "spotify.tokenUrl"),
+                defaultPlaylistId = get("SPOTIFY_DEFAULT_PLAYLIST_ID", "spotify.defaultPlaylistId")
+            )
         }
     }
 }
+
