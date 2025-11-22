a638eaeff1b4e533c112fa4feb83063d1fd02011
JL <115737916+yikesboy@users.noreply.github.com>
2025-11-22 18:01:10 +0100

    feat: updated HTTP cors config as requsted by colleagues working on the frontend

--- backend/Authentication-Service/src/main/kotlin/plugins/HTTP.kt
+++ backend/Authentication-Service/src/main/kotlin/plugins/HTTP.kt
@@ -15,6 +15,10 @@ fun Application.configureHTTP() {
         allowMethod(HttpMethod.Put)
         allowMethod(HttpMethod.Delete)
         allowMethod(HttpMethod.Patch)
+        allowMethod(HttpMethod.Post)
+        allowMethod(HttpMethod.Get)
+        allowHeader(HttpHeaders.ContentType)
+        allowCredentials = true
         allowHeader(HttpHeaders.Authorization)
         allowHeader("MyCustomHeader")
         anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
