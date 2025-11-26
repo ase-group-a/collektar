11f88e67170f189355fb4cfe22a993006156954a
Dominic Jelitsch <dominic.jelitsch@gmail.com>
2025-11-26 23:41:43 +0100

    fix: typo in game MediaItem id

--- backend/Media-Catalog-Service/src/main/kotlin/integration/igdb/IGDBMapper.kt
+++ backend/Media-Catalog-Service/src/main/kotlin/integration/igdb/IGDBMapper.kt
@@ -8,7 +8,7 @@ object IGDBMapper {
     fun gameToMediaItem(game: GameDto): MediaItem {
         
         return MediaItem(
-            id = "igdbb:${game.id}",
+            id = "igdb:${game.id}",
             title = game.name,
             type = MediaType.GAME,
             imageUrl = if (game.cover != null) "https://images.igdb.com/igdb/image/upload/t_cover_big/${game.cover.image_id}.webp" else null,
