e477649265b139da2a9d16f2cda9d93ad598f569
Sylvia Pisjak <sylviapi@edu.aau.at>
2025-11-27 10:16:32 +0100

    feat(google-books): implement Mapper to convert BookItemDto to MediaItem

---
+++ backend/Media-Catalog-Service/src/main/kotlin/integration/books/BooksMapper.kt
@@ -0,0 +1,19 @@
+package integration.books
+
+import domain.MediaItem
+import domain.MediaType
+
+object BookMapper {
+    fun bookToMediaItem(book: BookItemDto): MediaItem {
+        val info = book.volumeInfo
+
+        return MediaItem(
+            id = "google:book:${book.id}",
+            title = info.title ?: "Unknown title",
+            type = MediaType.BOOK,
+            imageUrl = info.imageLinks?.thumbnail,
+            description = info.authors?.joinToString(", ") ?: "Unknown author",
+            source = "google_books"
+        )
+    }
+}
