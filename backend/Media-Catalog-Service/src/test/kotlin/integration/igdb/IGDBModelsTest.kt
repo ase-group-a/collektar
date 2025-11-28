e5f2ed42b5e40c031acdc55b6b28c02fa7ccdabb
Dominic Jelitsch <dominic.jelitsch@gmail.com>
2025-11-28 20:31:53 +0100

    test: move common values to shared definition

--- backend/Media-Catalog-Service/src/test/kotlin/integration/igdb/IGDBModelsTest.kt
+++ backend/Media-Catalog-Service/src/test/kotlin/integration/igdb/IGDBModelsTest.kt
@@ -9,42 +9,14 @@ import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 import kotlin.test.assertNull
 
-const val GAME_ID = 233L
-const val COVER_ID = 77288L
-const val COVER_IMAGE_ID = "co1nmw"
-const val GAME_NAME = "Half-Life 2"
-const val GAME_SUMMARY = "1998. HALF-LIFE sends a shock through the game industry with its combination of pounding ..." // Shortened
 
 class IGDBModelsTest {
-    
-    // generated using the following query options:
-    // fields id, name, summary, cover.image_id; limit 1; offset 0; search "Half-Life 2";
-    // JSON array brackets have been removed, as deserialization of arrays will not be tested here
-    private val exampleResponseJson = """
-            {
-                "id": $GAME_ID,
-                "cover": {
-                    "id": $COVER_ID,
-                    "image_id": "$COVER_IMAGE_ID"
-                },
-                "name": "$GAME_NAME",
-                "summary": "$GAME_SUMMARY"
-            }
-    """.trimIndent()
-
-    // Same as previous, but with cover and summary missing, as this is possible for some games
-    private val exampleResponseMissingValuesJson = """
-            {
-                "id": $GAME_ID,
-                "name": "$GAME_NAME"
-            }
-    """.trimIndent()
 
     private val json = Json { encodeDefaults = true }
 
     @Test
     fun `GameDto deserializes correctly`() {
-        val deserialized = json.decodeFromString<GameDto>(exampleResponseJson)
+        val deserialized = json.decodeFromString<GameDto>(EXAMPLE_GAME_JSON)
         
         assertNotNull(deserialized.cover)
         assertNotNull(deserialized.summary)
@@ -57,7 +29,7 @@ class IGDBModelsTest {
 
     @Test
     fun `GameDto with null values deserializes correctly`() {
-        val deserialized = json.decodeFromString<GameDto>(exampleResponseMissingValuesJson)
+        val deserialized = json.decodeFromString<GameDto>(EXAMPLE_GAME_NULL_JSON)
         
         assertNull(deserialized.cover)
         assertNull(deserialized.summary)
@@ -75,7 +47,7 @@ class IGDBModelsTest {
             GAME_ID, coverDto, GAME_NAME, GAME_SUMMARY
         )
         val igdbGamesResponse = IGDBGamesResponse(
-            listOf(gameDto), 1
+            listOf(gameDto), EXAMPLE_IGDB_RESPONSE_X_COUNT
         )
         
         val serialized = json.encodeToString(igdbGamesResponse)
@@ -97,7 +69,7 @@ class IGDBModelsTest {
             GAME_ID, null, GAME_NAME, null
         )
         val igdbGamesResponse = IGDBGamesResponse(
-            listOf(gameDto), 1
+            listOf(gameDto), EXAMPLE_IGDB_RESPONSE_X_COUNT
         )
 
         val serialized = json.encodeToString(igdbGamesResponse)
---
+++ backend/Media-Catalog-Service/src/test/kotlin/integration/igdb/IGDBValues.kt
@@ -0,0 +1,65 @@
+package integration.igdb
+
+const val GAME_ID = 233L
+const val COVER_ID = 77288L
+const val COVER_IMAGE_ID = "co1nmw"
+const val GAME_NAME = "Half-Life 2"
+const val GAME_SUMMARY =
+    "1998. HALF-LIFE sends a shock through the game industry with its combination of pounding ..." // Shortened
+
+// generated using the following query options:
+// fields id, name, summary, cover.image_id; limit 1; offset 0; search "Half-Life 2";
+// JSON array brackets have been removed, as deserialization of arrays will not be tested here
+const val EXAMPLE_GAME_JSON = """
+{
+    "id": $GAME_ID,
+    "cover": {
+        "id": $COVER_ID,
+        "image_id": "$COVER_IMAGE_ID"
+    },
+    "name": "$GAME_NAME",
+    "summary": "$GAME_SUMMARY"
+}"""
+
+// Same as previous, but with cover and summary missing, as this is possible for some games
+const val EXAMPLE_GAME_NULL_JSON = """
+{
+    "id": $GAME_ID,
+    "name": "$GAME_NAME"
+}"""
+
+// Raw IGDB response with the following parameters:
+// fields id, name, summary, cover.image_id; limit 3; offset 1; search "Half-Life 2";
+const val EXAMPLE_IGDB_RESPONSE_JSON = """
+[
+    {
+        "id": 271414,
+        "cover": {
+            "id": 336850,
+            "image_id": "co77wy"
+        },
+        "name": "Half-Life 2: MMod - Half-Life 2: Episode One",
+        "summary": "This mod allows Half-Life 2: Episode One to be run under MMod.\n\nThe goal of Half-Life 2: MMod is to enhance and expand gunplay, combat mechanics and the immersion factor by giving the Player more options and combat opportunities as well as refine how the Player handles his arsenal. Half-Life 2: MMod also offers minor AI enhancements, extended abilities for combine soldiers ( dynamic jumping, firing smg1 underbarrel grenades, shotgunner double blast, etc. ), multiple bug fixes, enhanced visuals, VFX re-design, sound redesign and much much more, while keeping nearly every new feature in the mod totally optional."
+    },
+    {
+        "id": 271415,
+        "cover": {
+            "id": 336898,
+            "image_id": "co77ya"
+        },
+        "name": "Half-Life 2: MMod - Half-Life 2: Episode Two",
+        "summary": "This mod allows Episode Two to be run under MMod.\n\nThe goal of Half-Life 2: MMod is to enhance and expand gunplay, combat mechanics and the immersion factor by giving the Player more options and combat opportunities as well as refine how the Player handles his arsenal. Half-Life 2: MMod also offers minor AI enhancements, extended abilities for combine soldiers ( dynamic jumping, firing smg1 underbarrel grenades, shotgunner double blast, etc. ), multiple bug fixes, enhanced visuals, VFX re-design, sound redesign and much much more, while keeping nearly every new feature in the mod totally optional."
+    },
+    {
+        "id": 233,
+        "cover": {
+            "id": 77288,
+            "image_id": "co1nmw"
+        },
+        "name": "Half-Life 2",
+        "summary": "1998. HALF-LIFE sends a shock through the game industry with its combination of pounding action and continuous, immersive storytelling.\n\nNOW. By taking the suspense, challenge and visceral charge of the original, and adding startling new realism and responsiveness, Half-Life 2 opens the door to a world where the player's presence affects everything around them, from the physical environment to the behaviors even the emotions of both friends and enemies."
+    }
+]"""
+
+// Value of X-Count HTTP header provided by IGDB, which indicates the total count of found elements.
+const val EXAMPLE_IGDB_RESPONSE_X_COUNT = 44
