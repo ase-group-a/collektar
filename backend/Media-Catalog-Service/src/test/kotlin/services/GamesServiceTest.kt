ad0abd5e967691c19e106f14ecedb5c4bcac5c07
Dominic Jelitsch <dominic.jelitsch@gmail.com>
2025-11-30 18:14:04 +0100

    test: add test for GameService

---
+++ backend/Media-Catalog-Service/src/test/kotlin/services/GamesServiceTest.kt
@@ -0,0 +1,65 @@
+package services
+
+import com.collektar.integration.igdb.CoverDto
+import com.collektar.integration.igdb.GameDto
+import com.collektar.integration.igdb.IGDBClient
+import com.collektar.integration.igdb.IGDBGamesResponse
+import domain.MediaType
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
+import service.GamesService
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+
+// Values for testing
+const val QUERY = "query"
+const val OFFSET = 0
+const val LIMIT = 20
+const val TOTAL = 50
+const val GAME_ID = 12345L
+const val GAME_NAME = "Half-Life 3"
+const val GAME_SUMMARY = "Future game release"
+const val COVER_ID = 6789L
+const val COVER_IMAGE_ID = "a1b2c3d4"
+
+const val IGDB_SOURCE_NAME = "igdb"
+
+class GamesServiceTest {
+    private val igdbClient = mockk<IGDBClient>()
+    private val gamesService = GamesService(igdbClient)
+    
+    @Test
+    fun `search maps games correctly media items`() {
+        val response = IGDBGamesResponse(
+            items = listOf(
+                GameDto(
+                    GAME_ID,
+                    CoverDto (
+                        COVER_ID,
+                        COVER_IMAGE_ID,
+                    ),
+                    GAME_NAME,
+                    GAME_SUMMARY
+                )
+            ),
+            total = TOTAL
+        )
+        
+        coEvery { igdbClient.searchGames(QUERY, LIMIT, OFFSET) } returns response
+        
+        val res = runBlocking { gamesService.search(QUERY, LIMIT, OFFSET) }
+        
+        assertEquals(TOTAL, res.total)
+        assertEquals(response.items.count(), res.items.count())
+        
+        val item = res.items.first()
+        assertEquals("$IGDB_SOURCE_NAME:$GAME_ID", item.id )
+        assertEquals(GAME_NAME,item.title)
+        assertEquals(MediaType.GAME, item.type)
+        assertNotNull(item.imageUrl)
+        assertEquals(GAME_SUMMARY, item.description)
+        assertEquals(IGDB_SOURCE_NAME, item.source)
+    }
+}
