78bb13a29c457a8007516e05987141d5ac8823e6
JL <115737916+yikesboy@users.noreply.github.com>
2025-12-15 19:59:09 +0100

    feat: serialization config

--- backend/Email-Service/src/main/kotlin/plugins/Serialization.kt
+++ backend/Email-Service/src/main/kotlin/plugins/Serialization.kt
@@ -1,6 +1,20 @@
 package com.collektar.plugins
 
 import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonNamingStrategy
 
+@OptIn(ExperimentalSerializationApi::class)
 fun Application.configureSerialization() {
+    install(ContentNegotiation) {
+        json(Json {
+            prettyPrint = true
+            isLenient = true
+            ignoreUnknownKeys = true
+            namingStrategy = JsonNamingStrategy.SnakeCase
+        })
+    }
 }
