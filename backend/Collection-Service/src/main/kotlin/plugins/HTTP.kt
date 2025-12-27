7234e9a7313dcb6ac48c5449d1861898481c0d37
nicoszolderits <mail@nszo.net>
2025-12-27 02:09:22 +0100

    fix: added CORS settings on backend

--- backend/Collection-Service/src/main/kotlin/plugins/HTTP.kt
+++ backend/Collection-Service/src/main/kotlin/plugins/HTTP.kt
@@ -10,11 +10,17 @@ import io.ktor.server.routing.*
 fun Application.configureHTTP() {
     install(CORS) {
         allowMethod(HttpMethod.Options)
+        allowMethod(HttpMethod.Get)
+        allowMethod(HttpMethod.Post)
         allowMethod(HttpMethod.Put)
         allowMethod(HttpMethod.Delete)
         allowMethod(HttpMethod.Patch)
+
         allowHeader(HttpHeaders.Authorization)
-        allowHeader("MyCustomHeader")
+        allowHeader(HttpHeaders.ContentType)
+        allowHeader("X-User-Id")
+        allowCredentials = true
+
         anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
     }
     install(DefaultHeaders) {
