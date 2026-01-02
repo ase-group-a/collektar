a162f669717d84bef48d2dd959f6219d1e48ed0d
JL <115737916+yikesboy@users.noreply.github.com>
2026-01-02 17:26:17 +0100

    feat: added dependency injection setup and starting EmailConsumer in Application.kt

--- backend/Email-Service/src/main/kotlin/Application.kt
+++ backend/Email-Service/src/main/kotlin/Application.kt
@@ -1,10 +1,13 @@
 package com.collektar
 
+import com.collektar.consumer.IEmailConsumer
 import com.collektar.plugins.configureHTTP
 import com.collektar.plugins.configureMonitoring
 import com.collektar.plugins.configureRouting
 import com.collektar.plugins.configureSerialization
+import com.collektar.plugins.configureFrameworks
 import io.ktor.server.application.*
+import org.koin.ktor.ext.inject
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
@@ -15,4 +18,11 @@ fun Application.module() {
     configureMonitoring()
     configureHTTP()
     configureRouting()
+    configureFrameworks()
+
+    val emailConsumer by inject<IEmailConsumer>()
+    emailConsumer.start()
+    monitor.subscribe(ApplicationStopped) {
+        emailConsumer.stop()
+    }
 }
