6534cc9a2bc316a68ba8e0e15515111c035acfd3
JL <115737916+yikesboy@users.noreply.github.com>
2026-01-02 17:25:06 +0100

    refactor: renamed username -> displayName in EmailMessages to better align with our db schemas; renamed type -> emailType

--- backend/Email-Service/src/main/kotlin/models/EmailMessage.kt
+++ backend/Email-Service/src/main/kotlin/models/EmailMessage.kt
@@ -1,6 +1,9 @@
 package com.collektar.models
 
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonClassDiscriminator
 
 @Serializable
 enum class EmailType {
@@ -11,24 +14,28 @@ enum class EmailType {
     ACCOUNT_DELETED
 }
 
+
+@OptIn(ExperimentalSerializationApi::class)
 @Serializable
+@JsonClassDiscriminator("message_type")
 sealed class EmailMessage {
     abstract val to: String
-    abstract val type: EmailType
+    abstract val emailType: EmailType
     abstract fun buildContent(appUrl: String): EmailContent
 }
 
 @Serializable
+@SerialName("welcome_email")
 data class WelcomeEmail (
     override val to: String,
-    val username: String,
+    val displayName: String,
 ) : EmailMessage() {
-    override val type = EmailType.WELCOME
+    override val emailType = EmailType.WELCOME
     override fun buildContent(appUrl: String) = EmailContent (
         subject = "Welcome to Collektar!",
         title = "Welcome to Collektar!",
         body = """
-            <p>Hi $username,</p>
+            <p>Hi $displayName,</p>
             <p>We're thrilled to have you on board.</p>
             <p>You can now start exploring our platform and manage all your media in one place!</p>
             <a href="$appUrl" class="button">Get Started</a>
@@ -37,18 +44,19 @@ data class WelcomeEmail (
 }
 
 @Serializable
+@SerialName("password_reset_email")
 data class PasswordResetEmail(
     override val to: String,
-    val username: String,
+    val displayName: String,
     val resetToken: String,
     val expiryMinutes: Int = 60
 ) : EmailMessage() {
-    override val type = EmailType.PASSWORD_RESET
+    override val emailType = EmailType.PASSWORD_RESET
     override fun buildContent(appUrl: String) = EmailContent (
         subject = "Reset Your Password",
         title = "Reset Your Password",
         body = """
-            <p>Hi $username,</p>
+            <p>Hi $displayName,</p>
             <p>We received a request to reset your password. Click the button below to create a new password:</p>
             <a href="$appUrl/reset-password?token=$resetToken" class="button">Reset Password</a>
             <p>This link will expire in $expiryMinutes minutes.</p>
@@ -58,17 +66,18 @@ data class PasswordResetEmail(
 }
 
 @Serializable
+@SerialName("email_verification_email")
 data class EmailVerificationEmail(
     override val to: String,
-    val username: String,
+    val displayName: String,
     val verificationToken: String
 ) : EmailMessage() {
-    override val type = EmailType.EMAIL_VERIFICATION
+    override val emailType = EmailType.EMAIL_VERIFICATION
     override fun buildContent(appUrl: String) = EmailContent (
         subject = "Verify Your Email",
         title = "Verify Your Email",
         body = """
-            <p>Hi $username,</p>
+            <p>Hi $displayName,</p>
             <p>Thanks for signing up! Please verify your email address by clicking the button below:</p>
             <a href="$appUrl/verify-email?token=$verificationToken" class="button">Verify Email</a>
             <p>If you didn't create an account, you can safely ignore this email.</p>
@@ -77,16 +86,17 @@ data class EmailVerificationEmail(
 }
 
 @Serializable
+@SerialName("password_changed_email")
 data class PasswordChangedEmail(
     override val to: String,
-    val username: String
+    val displayName: String
 ) : EmailMessage() {
-    override val type = EmailType.PASSWORD_CHANGED
+    override val emailType = EmailType.PASSWORD_CHANGED
     override fun buildContent(appUrl: String) = EmailContent (
         subject = "Password Changed",
         title = "Password Changed",
         body = """
-            <p>Hi $username,</p>
+            <p>Hi $displayName,</p>
             <p>Your password has been changed successfully.</p>
             <p>If you didn't make this change, please contact our support team immediately.</p>
             <a href="$appUrl/support" class="button">Contact Support</a>
@@ -95,16 +105,17 @@ data class PasswordChangedEmail(
 }
 
 @Serializable
+@SerialName("account_deleted_email")
 data class AccountDeletedEmail(
     override val to: String,
-    val username: String
+    val displayName: String
 ) : EmailMessage() {
-    override val type = EmailType.ACCOUNT_DELETED
+    override val emailType = EmailType.ACCOUNT_DELETED
     override fun buildContent(appUrl: String) = EmailContent (
         subject = "Account Deleted",
         title = "Account Deleted",
         body = """
-            <p>Hi $username,</p>
+            <p>Hi $displayName,</p>
             <p>Your account has been successfully deleted as requested.</p>
             <p>We're sorry to see you go. If you change your mind, you're always welcome to create a new account.</p>
             <p>Thank you for being part of our community.</p>
