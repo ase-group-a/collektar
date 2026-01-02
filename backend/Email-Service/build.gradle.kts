d603edf6283b9f8171f0acc49f13429a38a03307
JL <115737916+yikesboy@users.noreply.github.com>
2026-01-02 17:21:35 +0100

    build: added kotlin serialization, ktor-serialization, rabbitmq-amqp, jarkata mail, koin and koin-logger

--- backend/Email-Service/build.gradle.kts
+++ backend/Email-Service/build.gradle.kts
@@ -1,10 +1,13 @@
 val kotlin_version: String by project
+val koin_version: String by project
 val logback_version: String by project
 val prometheus_version: String by project
+val ktor_version: String by project
 
 plugins {
     kotlin("jvm") version "2.2.21"
     id("io.ktor.plugin") version "3.3.2"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.20"
 }
 
 group = "com.collektar"
@@ -27,4 +30,10 @@ dependencies {
     implementation("io.ktor:ktor-server-config-yaml")
     testImplementation("io.ktor:ktor-server-test-host")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
+    implementation("com.rabbitmq:amqp-client:5.28.0")
+    implementation("com.sun.mail:jakarta.mail:2.0.2")
+    implementation("io.insert-koin:koin-ktor:$koin_version")
+    implementation("io.insert-koin:koin-logger-slf4j:$koin_version")
 }
