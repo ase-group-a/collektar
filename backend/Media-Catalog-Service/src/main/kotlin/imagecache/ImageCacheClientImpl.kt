ec2fc781f7d52e63938a73fabc775bd013bd83f1
Dominic Jelitsch <dominic.jelitsch@gmail.com>
2026-01-03 22:53:32 +0100

    feat: add image cache retrieving and storing

--- backend/Media-Catalog-Service/src/main/kotlin/imagecache/ImageCacheClientImpl.kt
+++ backend/Media-Catalog-Service/src/main/kotlin/imagecache/ImageCacheClientImpl.kt
@@ -1,6 +1,10 @@
 package com.collektar.imagecache
 
 import io.ktor.client.HttpClient
+import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsBytes
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.isSuccess
 import redis.clients.jedis.ConnectionPoolConfig
 import redis.clients.jedis.RedisClient
 import java.time.Duration
@@ -43,7 +47,38 @@ class ImageCacheClientImpl(
             throw IllegalStateException("Redis connection is unavailable, unable to serve images.")
         }
         
+        val imageKey = "$imageSource:$imageId".toByteArray()
+        
+        // Get image from Redis cache
+        var image = redis!!.get(imageKey)
+        if (image == null) {
+            // Image does not exist in the cache, retrieve it from the external service
+            image = getImageFromService(imageSource, imageId)
+            redis!!.set(imageKey, image)
+        }
+        
+        return image
+    }
 
+    /**
+     * Retrieves the image from the external service
+     */
+    private suspend fun getImageFromService (imageSource: ImageSource, imageId: String) : ByteArray {
+        val imageUrl = when (imageSource) {
+            ImageSource.SPOTIFY -> "${config.spotifyUrlPrefix}$imageId"
+            ImageSource.TMBD -> "${config.tmdbUrlPrefix}$imageId.jpg"
+            ImageSource.IGDB -> "${config.igdbUrlPrefix}$imageId.jpg"
+            ImageSource.GOOGLE_BOOKS -> "${config.googleBooksUrlPrefix}$imageId"
+        }
+        
+        val response = httpClient.get(imageUrl)
+        
+        if (!response.status.isSuccess()) {
+            val text = response.bodyAsText()
+            throw RuntimeException("Failed to fetch image $imageId from service $imageSource: $text")
+        }
+        
+        return response.bodyAsBytes()
     }
 
     override fun getImageUrl(
