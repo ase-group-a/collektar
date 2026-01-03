b5617e5dad7c3099fadef0b54a4aa07e41f38a4f
JL <115737916+yikesboy@users.noreply.github.com>
2026-01-03 11:31:08 +0100

    refactor: improved the testability of EmailTemplateLoader

--- backend/Email-Service/src/main/kotlin/shared/utility/EmailTemplateLoader.kt
+++ backend/Email-Service/src/main/kotlin/shared/utility/EmailTemplateLoader.kt
@@ -3,7 +3,9 @@ package com.collektar.shared.utility
 import com.collektar.config.TemplateLoaderConfig
 
 class EmailTemplateLoader(
-    private val config: TemplateLoaderConfig
+    private val config: TemplateLoaderConfig,
+    private val classLoader: ClassLoader = EmailTemplateLoader::class.java.classLoader
+        ?: throw IllegalStateException("ClassLoader not available")
 ): IEmailTemplateLoader {
     private var templateCache = mutableMapOf<String, String>()
 
@@ -16,12 +18,9 @@ class EmailTemplateLoader(
     private fun loadTemplateContent(templateName: String): String {
         val resourcePath = "${config.templateFolder}/$templateName"
 
-        return this::class.java.classLoader
-            ?.getResourceAsStream(resourcePath)
-            ?.bufferedReader()
-            ?.use { it.readText() }
-            ?: throw IllegalStateException(
-                "Template '$templateName' not found at path: $resourcePath"
-            )
+        val resourceStream = classLoader.getResourceAsStream(resourcePath)
+            ?: throw IllegalStateException("Template '$templateName' not found at path: $resourcePath")
+
+        return resourceStream.bufferedReader().use { it.readText() }
     }
 }
