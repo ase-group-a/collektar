8531b0370d4c967fde4b48258d784cf6f5a719df
Dominic Jelitsch <dominic.jelitsch@gmail.com>
2026-01-03 20:36:41 +0100

    feat: update redis connection env variables

--- backend/Media-Catalog-Service/src/main/kotlin/imagecache/ImageCacheConfig.kt
+++ backend/Media-Catalog-Service/src/main/kotlin/imagecache/ImageCacheConfig.kt
@@ -1,13 +1,12 @@
 package com.collektar.imagecache
 
 import com.collektar.config.ConfigUtils.getConfigValue
-import com.collektar.integration.shared.OauthConfig
 import io.ktor.server.application.ApplicationEnvironment
 
 data class ImageCacheConfig (
     // The url Prefix is used to generate the URL that is sent to the client within the MediaItem
     val urlPrefix: String,
-    val redisPassword: String,
+    val redisUrl: String,
     // Below are prefixes for image URLs
     val spotifyUrlPrefix: String,
     val tmdbUrlPrefix: String,
@@ -18,7 +17,7 @@ data class ImageCacheConfig (
         fun fromEnv(env: ApplicationEnvironment): ImageCacheConfig {
             return ImageCacheConfig(
                 urlPrefix = getConfigValue(env, "IMAGE_CACHE_URL_PREFIX", "imageCache.urlPrefix"),
-                redisPassword = getConfigValue(env, "IMAGE_CACHE_REDIS_PASSWORD", "imageCache.redisPassword"),
+                redisUrl = getConfigValue(env, "IMAGE_CACHE_REDIS_URL", "imageCache.redisUrl"),
                 
                 spotifyUrlPrefix = getConfigValue(env, "SPOTIFY_IMAGE_URL", "imageCache.spotifyUrlPrefix"),
                 tmdbUrlPrefix = getConfigValue(env, "TMDB_IMAGE_URL", "imageCache.tmdbUrlPrefix"),
