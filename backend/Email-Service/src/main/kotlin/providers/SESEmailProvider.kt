919bfb047e70663e3dd9554e28efa3e12d8cc48b
JL <115737916+yikesboy@users.noreply.github.com>
2026-01-03 17:13:54 +0100

    refactor: using common IMailSession and IMailTransport instead of libaray implemenation in SESEmailProvider

--- backend/Email-Service/src/main/kotlin/providers/SESEmailProvider.kt
+++ backend/Email-Service/src/main/kotlin/providers/SESEmailProvider.kt
@@ -1,85 +1,62 @@
 package com.collektar.providers
 
 import com.collektar.config.EmailProviderConfig
+import com.collektar.providers.mail.IMailSession
+import com.collektar.providers.mail.IMailTransport
+import com.collektar.providers.mail.JakartaMailSession
+import com.collektar.providers.mail.JakartaMailTransport
 import jakarta.mail.*
-import jakarta.mail.internet.InternetAddress
-import jakarta.mail.internet.MimeBodyPart
-import jakarta.mail.internet.MimeMessage
-import jakarta.mail.internet.MimeMultipart
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.util.*
 
 class SESEmailProvider(
-    private val config: EmailProviderConfig
+    private val config: EmailProviderConfig,
+    private val mailSession: IMailSession = createDefaultSession(config),
+    private val mailTransport: IMailTransport = JakartaMailTransport()
 ) : IEmailProvider {
-    private val session: Session by lazy { createSession(config) }
-
     override suspend fun sendEmail(
         to: String,
         subject: String,
         htmlBody: String
     ) : Result<Unit> = withContext(Dispatchers.IO) {
         try {
-            val message = createMessage(
-                session = session,
-                to = to,
-                subject = subject,
-                htmlBody = htmlBody
-            )
-            Transport.send(message)
+            val message = mailSession.createMessage()
+            message.setSubject(subject, "UTF-8")
+            message.setFrom(config.fromEmail, config.fromName)
+            message.setRecipients(to)
+            message.setHtmlContent(htmlBody)
+            message.setSentDate()
+
+            mailTransport.send(message)
+
             Result.success(Unit)
         } catch (e: Exception) {
             Result.failure(e)
         }
     }
 
-    private fun createSession(config: EmailProviderConfig): Session {
-        return Session.getInstance(
-            createSmtpProperties(config),
-            createAuthenticator(config)
-        )
-    }
+    companion object {
+        private fun createDefaultSession(config: EmailProviderConfig): IMailSession {
+            val properties = Properties().apply {
+                put("mail.smtp.auth", true)
+                put("mail.smtp.host", config.host)
+                put("mail.smtp.port", config.port)
+                put("mail.smtp.starttls.enable", "true")
+                put("mail.smtp.starttls.required", "true")
+            }
 
-    private fun createSmtpProperties(config: EmailProviderConfig) = Properties().apply {
-            put("mail.smtp.auth", true)
-            put("mail.smtp.host", config.host)
-            put("mail.smtp.port", config.port)
-            put("mail.smtp.starttls.enable", "true")
-            put("mail.smtp.starttls.required", "true")
-    }
+            val authenticator = object : Authenticator() {
+                override fun getPasswordAuthentication(): PasswordAuthentication {
+                    return PasswordAuthentication(
+                        config.username,
+                        config.password
+                    )
+                }
+            }
 
-    private fun createAuthenticator(config: EmailProviderConfig) = object : Authenticator() {
-        override fun getPasswordAuthentication(): PasswordAuthentication {
-            return PasswordAuthentication(
-                config.username,
-                config.password
-            )
+            val session = Session.getInstance(properties, authenticator)
+            return JakartaMailSession(session)
         }
-    }
-
-    private fun createMessage(
-        session: Session,
-        to: String,
-        subject: String,
-        htmlBody: String
-    ) = MimeMessage(session).apply {
-        setSubject(subject, "UTF-8")
-        setFrom(InternetAddress(config.fromEmail, config.fromName))
-        setRecipients(
-            Message.RecipientType.TO,
-            InternetAddress.parse(to)
-        )
-
-        val multipart = MimeMultipart("alternative")
-        val htmlPart = createBodyPart(htmlBody)
-
-        multipart.addBodyPart(htmlPart)
-        setContent(multipart)
-        sentDate = Date()
-    }
-
-    private fun createBodyPart(htmlBody: String) = MimeBodyPart().apply {
-        setContent(htmlBody, "text/html; charset=UTF-8")
     }
 }
