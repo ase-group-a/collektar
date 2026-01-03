59007286708283f08832a54312af02a7c009e968
JL <115737916+yikesboy@users.noreply.github.com>
2026-01-03 23:28:56 +0100

    refactor: improved testability of EmailMessageProcessor

--- backend/Email-Service/src/main/kotlin/consumer/processor/EmailMessageProcessor.kt
+++ backend/Email-Service/src/main/kotlin/consumer/processor/EmailMessageProcessor.kt
@@ -8,12 +8,14 @@ import java.nio.charset.StandardCharsets
 
 class EmailMessageProcessor(
     private val emailBuilder: IEmailBuilder,
-    private val emailSender: IEmailSender
+    private val emailSender: IEmailSender,
+    private val json: Json = Json { ignoreUnknownKeys = false; classDiscriminator = "message_type" }
 ) : IEmailMessageProcessor {
     override suspend fun process(messageBody: ByteArray): ProcessingResult {
         try {
             val messageJson = String(messageBody, StandardCharsets.UTF_8)
-            val emailMessage = Json.decodeFromString<EmailMessage>(messageJson)
+            val emailMessage = json.decodeFromString<EmailMessage>(messageJson)
+
             val email = emailBuilder.buildEmail(emailMessage)
 
             return emailSender.send(email).fold(
