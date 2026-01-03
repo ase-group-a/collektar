7f9fd8ce8cce56df3b60043197bb3cc9295bf617
Dominic Jelitsch <dominic.jelitsch@gmail.com>
2026-01-03 21:00:39 +0100

    feat: add media image url parser

--- backend/Media-Catalog-Service/src/main/kotlin/imagecache/ImageUrlParser.kt
+++ backend/Media-Catalog-Service/src/main/kotlin/imagecache/ImageUrlParser.kt
@@ -1,18 +1,36 @@
 package com.collektar.imagecache
 
+import io.ktor.http.*
+
 object ImageUrlParser {
-    fun extractSpotifyImageId(url: String) : String {
-        TODO("Not yet implemented")
-        // Example URL: "https://i.scdn.co/image/ab67616d0000b273d7812467811a7da6e6a44902"
+    fun extractSpotifyImageId(url: String): String {
+        val parsedUrl = Url(url)
+
+        // Extract the last path segment of the URL, which is the id
+        // Example URL: "https://i.scdn.co/image/IMG_ID_HERE"
+        val id = parsedUrl.segments.last()
+        return id
     }
 
-    fun extractTMDBImageId(url: String) : String {
-        TODO("Not yet implemented")
-        // Example URL: "g96wHxU7EnoIFwemb2RgohIXrgW.jpg"
+    fun extractTMDBImageId(url: String): String {
+        // Remove prefix '/' character and image format postfix
+        // Example URL: "/IMG_ID_HERE.jpg"
+        val filename = url.drop(1)
+        val id = filename.split(".").first()
+        return id
     }
 
-    fun extractGoogleBooksImageId(url: String) : String {
-        TODO("Not yet implemented")
-        // Example URL: "http://books.google.com/books/content?id=pn-NVZ18yKkC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+    fun extractGoogleBooksImageId(url: String): String {
+        val parsedUrl = Url(url)
+
+        // Get id from query parameters
+        // Example URL: "http://books.google.com/books/content?id=IMG_ID_HERE&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+        val id = parsedUrl.parameters["id"]
+
+        if (id != null) {
+            return id
+        } else {
+            throw IllegalArgumentException("Provided Google Books Image URL does not match the specified format.")
+        }
     }
 }
