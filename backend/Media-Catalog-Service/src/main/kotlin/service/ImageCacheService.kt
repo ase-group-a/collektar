0d27d8d1cdf194d612831ded90b63aa1f70ad63a
Dominic Jelitsch <dominic.jelitsch@gmail.com>
2026-01-03 21:09:59 +0100

    feat: add ImageCacheService

--- backend/Media-Catalog-Service/src/main/kotlin/service/ImageCacheService.kt
+++ backend/Media-Catalog-Service/src/main/kotlin/service/ImageCacheService.kt
@@ -1,6 +1,7 @@
 package com.collektar.service
 
 import com.collektar.imagecache.ImageCacheClient
+import com.collektar.imagecache.ImageSource
 
 class ImageCacheService (
     private val imageCacheClient: ImageCacheClient
@@ -10,6 +11,7 @@ class ImageCacheService (
             throw IllegalArgumentException("Image source or id cannot be null.")
         }
         
-        TODO("Not implemented yet")
+        val imageSourceEnum = ImageSource.valueOf(imageSource.uppercase())
+        return imageCacheClient.getImage(imageSourceEnum, id)
     }
 }
