ac58047fb369af95a445c7be8a44c68c2e668200
Dominic Jelitsch <dominic.jelitsch@gmail.com>
2026-01-04 22:59:41 +0100

    test: add initial cypress test
    collections service is not included in health check, as it requires authentication.

---
+++ cypress/e2e/general/health.cy.js
@@ -0,0 +1,31 @@
+describe('GET /api/media/health', () => {
+    beforeEach(() => {
+        cy.fixture('general.json').as('general');
+    });
+
+    it('should return 200 and OK for /api/media/health', function () {
+        cy.request({
+            method: 'GET',
+            url: `${this.general.mediaApi}/health`
+        }).then((response) => {
+            expect(response.status).to.equal(200);
+            expect(response.body).to.equal('OK');
+        });
+    });
+});
+
+describe('GET /api/auth/health', () => {
+    beforeEach(() => {
+        cy.fixture('general.json').as('general');
+    });
+
+    it('should return 200 and OK for /api/auth/health', function () {
+        cy.request({
+            method: 'GET',
+            url: `${this.general.authApi}/health`
+        }).then((response) => {
+            expect(response.status).to.equal(200);
+            expect(response.body).to.equal('OK');
+        });
+    });
+});
