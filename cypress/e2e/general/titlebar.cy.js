58447f142d365ace1e47660235107edf423fca01
Dominic Jelitsch <dominic.jelitsch@gmail.com>
2026-01-05 21:07:41 +0100

    test: separate title bar tests

--- cypress/e2e/general/titlebar.cy.js
+++ cypress/e2e/general/titlebar.cy.js
@@ -10,46 +10,60 @@ describe('Title bar displays correctly', () => {
         cy.get('.navbar')
             .should("exist")
             .should("not.be.empty")
+    })
 
+    it('Image is displaying', () => {
         cy.get('.navbar')
             .find("img")
             .should("exist")
             .should("have.attr", "src", TITLE_BAR_SETTINGS.logoSrc)
+    })
 
+    it('Games link is displaying', () => {
         cy.get('.navbar')
             .find(`a[href="${TITLE_BAR_SETTINGS.games.href}"]`)
             .should("exist")
             .should("not.be.empty")
             .contains(TITLE_BAR_SETTINGS.games.name)
+    })
 
+    it('Movies link is displaying', () => {
         cy.get('.navbar')
             .find(`a[href="${TITLE_BAR_SETTINGS.movies.href}"]`)
             .should("exist")
             .should("not.be.empty")
             .contains(TITLE_BAR_SETTINGS.movies.name)
+    })
 
+    it('Shows link is displaying', () => {
         cy.get('.navbar')
             .find(`a[href="${TITLE_BAR_SETTINGS.shows.href}"]`)
             .should("exist")
             .should("not.be.empty")
             .contains(TITLE_BAR_SETTINGS.shows.name)
+    })
 
+    it('Music link is displaying', () => {
         cy.get('.navbar')
             .find(`a[href="${TITLE_BAR_SETTINGS.music.href}"]`)
             .should("exist")
             .should("not.be.empty")
             .contains(TITLE_BAR_SETTINGS.music.name)
+    })
 
+    it('Books link is displaying', () => {
         cy.get('.navbar')
             .find(`a[href="${TITLE_BAR_SETTINGS.books.href}"]`)
             .should("exist")
             .should("not.be.empty")
             .contains(TITLE_BAR_SETTINGS.books.name)
+    })
 
+    it('Login button is displaying', () => {
         cy.get('.navbar')
             .find(`.btn`)
             .should("exist")
             .should("not.be.empty")
             .contains(TITLE_BAR_SETTINGS.login.name)
-    });
+    })
 })
