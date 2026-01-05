5d00ef5ac71ae7a4af95365bc6458b09e3186a08
Dominic Jelitsch <dominic.jelitsch@gmail.com>
2026-01-05 20:03:50 +0100

    test: update cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,16 @@
-const { defineConfig } = require("cypress");
+const {defineConfig} = require('cypress');
 
 module.exports = defineConfig({
-  projectId: "6gffrg",
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    projectId: '6gffrg',
+    e2e: {
+        baseUrl: 'http://localhost',
+        retries: {
+            runMode: 3,
+        },
+        viewportHeight: 1080,
+        viewportWidth: 1920,
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
     },
-  },
 });
