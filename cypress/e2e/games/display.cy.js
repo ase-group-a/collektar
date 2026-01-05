030c40a87ca1cee4c1f65d56d900a116e98079cd
Dominic Jelitsch <dominic.jelitsch@gmail.com>
2026-01-05 21:02:01 +0100

    test: games display

---
+++ cypress/e2e/games/display.cy.js
@@ -0,0 +1,26 @@
+describe('/media/games displays correctly', () => {
+
+    it('should correctly navigate to games and display at least one media item', () => {
+        // Visit homepage
+        cy.visit('/')
+
+        // Find and search games button
+        cy.get('a[href="/media/games"]')
+            .should('exist')
+            .click()
+
+        // At least one card exists
+        cy.get('.card').first()
+            .should('exist')
+            .find('img')
+            .should('have.attr', 'src')
+            .and("not.be.empty")
+
+        cy.get('.card').first()
+            .should('exist')
+            .find('h2')
+            .should('exist')
+            .invoke('text')
+            .should('not.be.empty')
+    })
+})
